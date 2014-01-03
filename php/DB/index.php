1a7f90bfc3a83e23b9dee9de71c2f782db0ce294
sudosiyang <sudosiyang@gmail.com>
2014-01-04 01:15:19 +0800

    compess js delete PHP

--- php/DB/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "DataBase.php";
-$user=new DataBase();
-$sql="select * from user";
-$user->execute($sql);
-$result=$user->fetchArray();
-echo $result["uid"];
-?>
--- php/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'DB/valueGet.php';
-include_once 'DB/fileModel.php';
-$fileIfo = new fileModel();
-$fileIfo->scanNumAdd(29);
-?>
