5983c3209b549ccb336026204c3a5bce846f6af5
sudosiyang <744276721@qq.com>
2014-01-13 10:38:30 +0800

    change

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
      var $ = require('zepto');
      var gmu = require('./gmu');
      var Login = require("./model/login");
-         window.scrollTo(0, 1); //收起地址栏
-         //确认是否登录过
-         Login.init().checkLogin();
+     window.scrollTo(0, 1); //收起地址栏
+     //确认是否登录过
+     Login.init().checkLogin();
  })
